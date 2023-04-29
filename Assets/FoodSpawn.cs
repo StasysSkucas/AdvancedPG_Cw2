07b16227359b1ac3f92796bd868b652e267322db
StasZappar <stasys.skucas@zappar.com>
2023-04-29 16:51:59 +0300

    FoodSpawnFix

--- Assets/FoodSpawn.cs
+++ Assets/FoodSpawn.cs
@@ -5,13 +5,19 @@ using UnityEngine;
 
 public class FoodSpawn : MonoBehaviour
 {
-    public BoidManager bm;
+    public FinalBoidManager bm;
     public GameObject foodPrefab;
     public List<GameObject> spawnedFood = new List<GameObject>();
     public bool FoodSpawned = false;
 
+    private void Start()
+    {
+  
+    }
+
     public void SpawnFood(GameObject foodPrefab)
     {
+        bm = GameObject.Find("FinalBoidManager(Clone)").GetComponent<FinalBoidManager>();
         Vector3 foodPos = bm.transform.position + new Vector3(UnityEngine.Random.Range(-bm.TankSize, bm.TankSize),
                                                               UnityEngine.Random.Range(-bm.TankSize, bm.TankSize),
                                                               UnityEngine.Random.Range(-bm.TankSize, bm.TankSize));
@@ -20,6 +26,7 @@ public class FoodSpawn : MonoBehaviour
         FOBJ.transform.localPosition = foodPos;
         bm.SetFoodDestination(foodPos);
         spawnedFood.Add(FOBJ);
+        //Destroy(FOBJ, 4f); TURN ON LATER
     }
 
 }
